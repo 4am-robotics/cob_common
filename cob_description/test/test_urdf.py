70a2f4ae3b5167d2baffcd77fe5364200e8d984c
ipa-fxm <felix.messmer@ipa.fhg.de>
2015-08-09 16:18:23 +0200

    remove trailing whitespaces

--- cob_description/test/test_urdf.py
+++ cob_description/test/test_urdf.py
@@ -14,13 +14,13 @@ class TestUrdf(unittest.TestCase):
 	def test_correct_format(self):
 		print sys.argv
 		print len(sys.argv)
-	
+
 		if len(sys.argv) < 2:
 			self.fail("no urdf file given, usage: " + os.path.basename(sys.argv[0]) + " file.urdf.xacro. \ninput parameters are: " + str(sys.argv))
-	
+
 		file_to_test = sys.argv[1]
 		print "testing " + file_to_test
-		
+
 		# check if file exists
 		if not os.path.exists(file_to_test):
 			self.fail('file "' + file_to_test + '" not found')
@@ -34,6 +34,6 @@ class TestUrdf(unittest.TestCase):
 		p = subprocess.Popen("check_urdf /tmp/test.urdf", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 		if p.returncode != 0 and p.returncode != None:
 			self.fail("urdf not correct. file: " + file_to_test + "\n" + p.stderr.read())
- 
+
 if __name__ == '__main__':
-	rosunit.unitrun('cob_description', 'test_urdf', TestUrdf) 
+	rosunit.unitrun('cob_description', 'test_urdf', TestUrdf)
