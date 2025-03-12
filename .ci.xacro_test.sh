a146ea739c74a2f64d228b1c8ee294d25f6ac205
Majd Khalife <majd.skhalife@gmail.com>
2025-03-12 13:17:19 -0400

    humble

--- .ci.xacro_test.sh
+++ .ci.xacro_test.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 status=0
-[ "$ROS_DISTRO" \< "melodic" ] && xacro_args='--inorder'
-for x in $(find "$TARGET_REPO_PATH" -name '*.xacro'|sort); do
+# Only set --inorder if running on older ROS 1 distros; for ROS 2 (Humble) we don't need it.
+if [ -n "$ROS_DISTRO" ] && { [ "$ROS_DISTRO" = "indigo" ] || [ "$ROS_DISTRO" = "jade" ] || [ "$ROS_DISTRO" = "kinetic" ]; }; then
+    xacro_args='--inorder'
+else
+    xacro_args=''
+fi
+
+for x in $(find "$TARGET_REPO_PATH" -name '*.xacro' | sort); do
     echo "Testing $x"
     xacro $xacro_args "$x" > /dev/null || status=1
 done
